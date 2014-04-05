c0f335ba78f95ad3a1ea26d9b7fc2f8854fd34e0
chris-maroudas <mallibu97@gmail.com>
2014-04-06 00:45:13 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectEve::Application.config.secret_token = 'ea7cee5f42c21a64c054c43a2bdbf08b1952d5f7b95fac32bf2eaa40452dbbbe45736a8a8a15a4189edf879a4b492af9ea82b3c399779278f8c900aa579f9e7f'
