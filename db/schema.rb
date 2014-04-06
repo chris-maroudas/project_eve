c31f919d19e68f18a8b50fa9078dca11d10dbd0d
chris-maroudas <mallibu97@gmail.com>
2014-04-06 06:40:31 +0300

    reverted test migration

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,4 @@
 
 ActiveRecord::Schema.define(:version => 20140405215840) do
 
-  create_table "malakas", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
 end
