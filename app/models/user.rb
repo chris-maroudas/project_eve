6fa1a24540368581fa093b2efe6258ee19d6bba9
chris-maroudas <mallibu97@gmail.com>
2014-04-10 20:54:47 +0300

    Add birth_date to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :nickname, :image_url, :steam_user
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :nickname, :image_url, :steam_user, :birth_date
   # attr_accessible :title, :body
 
   has_one :steam_metadata, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140410041249) do
+ActiveRecord::Schema.define(:version => 20140410175042) do
 
   create_table "steam_metadata", :force => true do |t|
     t.string   "uid"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20140410041249) do
     t.string   "nickname"
     t.string   "image_url"
     t.boolean  "steam_user"
+    t.date     "birth_date"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
